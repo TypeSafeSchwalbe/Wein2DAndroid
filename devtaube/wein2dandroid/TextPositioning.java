5605dc3207d4ac2dbaea13b576b3f7de57af050f
DevTaube <78918080+DevTaube@users.noreply.github.com>
2022-04-12 20:31:52 +0200

    Delete devtaube/wein2dandroid directory

--- devtaube/wein2dandroid/TextPositioning.java
+++
@@ -1,8 +0,0 @@
-package devtaube.wein2dandroid;
-
-public final class TextPositioning
-{
-    public static final int LEFT = 0;
-    public static final int CENTER = 1;
-    public static final int RIGHT = 2;
-}
